192773dcee052925e863bd44ea3f375b502911a3
vlad94568 <vlad94568@gmail.com>
2018-12-31 10:51:15 -0800

    Added leafs on flowers.

--- src/scene/flower.py
+++ src/scene/flower.py
@@ -24,10 +24,15 @@ class Flower(SceneElement):
         self.stem_color = DARK_GREEN_COLOR
 
     def draw(self, screen):
+        # Drawing petals.
         pygame.draw.rect(screen, self.petal_color, (self.x, 427 - self.stem_height + 10, 10, 10))
         pygame.draw.rect(screen, self.petal_color, (self.x - 10, 417 - self.stem_height + 10, 10, 10))
         pygame.draw.rect(screen, self.petal_color, (self.x - 10, 437 - self.stem_height + 10, 10, 10))
         pygame.draw.rect(screen, self.petal_color, (self.x - 20, 427 - self.stem_height + 10, 10, 10))
+        # Drawing center of flower.
         pygame.draw.rect(screen, self.center_color, (self.x - 10, 427 - self.stem_height + 10, 10, 10))
+        # Drawing stem.
         pygame.draw.rect(screen, self.stem_color, (self.x - 7, 447 - self.stem_height + 10, 4, self.stem_height + 10))
-        print(self.stem_height)
+        # Drawing leaves.
+        pygame.draw.rect(screen, self.stem_color, (self.x - 17, 425 + (self.stem_height % 2), 23, 8))
+
